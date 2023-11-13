31fd36d402fedc5f67c2fcb48c424eb5dd601822
IttaYoshi <ittalian0329@gmail.com>
2023-11-14 03:39:18 +0900

    名刺編集機能実装

--- app/src/main/java/com/example/ittalian/outofbusinesscards/EditActivity.kt
+++ app/src/main/java/com/example/ittalian/outofbusinesscards/EditActivity.kt
@@ -9,6 +9,7 @@ class EditActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         PreferenceManager.getDefaultSharedPreferences(this).apply {
--- app/src/main/java/com/example/ittalian/outofbusinesscards/MainActivity.kt
+++ app/src/main/java/com/example/ittalian/outofbusinesscards/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.ittalian.outofbusinesscards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.preference.PreferenceManager
+import com.example.ittalian.outofbusinesscards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).apply {
+            val company = getString("company", "")
+            val postal = getString("postal", "")
+            val address= getString("address", "")
+            val tel = getString("tel", "")
+            val fax = getString("fax", "")
+            val email = getString("email", "")
+            val url = getString("url", "")
+            val position = getString("position", "")
+            val name = getString("name", "")
+
+            binding.companyText.text = company
+            binding.postalText.text = postal
+            binding.addressText.text = address
+            binding.telText.text = "tel:" + tel
+            binding.faxText.text = "fax:" + fax
+            binding.emailText.text = email
+            binding.urlText.text = url
+            binding.positionText.text = position
+            binding.nameText.text = name
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId){
+            R.id.edit -> {
+                val intent = Intent(this, EditActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
